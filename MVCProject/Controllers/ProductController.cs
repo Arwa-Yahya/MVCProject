6d33a4577d6f2e117b61277bf61182d6b03033f2
Arwa-Yahya <arwaayahya11@gmail.com>
2026-06-08 21:10:59 +0300

    MVCProject

---
+++ MVCProject/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductBL productBL = new ProductBL();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            List<Product> products = productBL.GetAll();
+            return View("ShowAll", products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = productBL.GetById(id);
+            return View(product);
+        }
+    }
+}
---
+++ MVCProject/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string ImageURL { get; set; }
+        public string Description { get; set; }
+    }
+}
