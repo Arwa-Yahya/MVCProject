8e80e8646ca3f2a457c1d64e0bda01cafe2c6635
Arwa-Yahya <arwaayahya11@gmail.com>
2026-06-22 16:13:30 +0300

    Add Course and Tag Helpers in view

---
+++ MVCProject/Controllers/CourseController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCProject.Models;
+using MVCProject.ViewModels;
+using System.Collections;
+
+namespace MVCProject.Controllers
+{
+    public class CourseController : Controller
+    {
+        StepsContext context = new StepsContext();
+        public IActionResult Index()
+        {
+            var courses = context.Courses
+                 .Include(i => i.Instructors)
+                 .ToList();
+
+            return View(courses);
+        }
+
+        public IActionResult New()
+        {
+            CrsInfoExtraInfoViewModel model = new CrsInfoExtraInfoViewModel();
+
+            model.InstructorList = context.Instructors.ToList();
+            model.DeptList = context.Departments.ToList();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SaveNew(CrsInfoExtraInfoViewModel crsfromreq)
+        {
+            if (crsfromreq.CrsName != null)
+            {
+                Course course = new Course();
+
+                course.Name = crsfromreq.CrsName;
+                course.Degree = crsfromreq.CrsDegree;
+                course.MinDegree = crsfromreq.CrsMinDegree;
+                course.Hours = crsfromreq.CrsHours;
+                course.Dept_Id = crsfromreq.DepartmentId;
+                course.Instructors = context.Instructors
+                .Where(x => x.Id == crsfromreq.InstructorId)
+               .ToList();
+
+                context.Courses.Add(course);
+                context.SaveChanges();
+
+                return RedirectToAction("Index", "Course");
+            }
+
+            return View("New", crsfromreq);
+        }
+    }
+}
---
+++ MVCProject/ViewModels/CrsInfoExtraInfoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MVCProject.Models;
+
+namespace MVCProject.ViewModels
+{
+    public class CrsInfoExtraInfoViewModel
+    {
+        public int CrsId { get; set; }
+        public string CrsName { get; set; }
+        public int CrsDegree { get; set; }
+        public int CrsMinDegree { get; set; }
+        public int CrsHours { get; set; }
+
+        public int DepartmentId { get; set; }
+        public List<Department> DeptList { get; set; }
+
+        public int InstructorId { get; set; }
+        public List<Instructor> InstructorList { get; set; }
+       
+
+
+
+
+    }
+}
