84525c71dbb5d56ae091b03ea5e452d34d41e035
Arwa-Yahya <arwaayahya11@gmail.com>
2026-07-10 01:36:50 +0300

    Layout

---
+++ MVCProject/Views/Shared/_NavbarPV.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCProject.Views.Shared
+{
+    public class _NavbarPVModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MVCProject/Views/Shared/_SidebarPV.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCProject.Views.Shared
+{
+    public class _SidebarPVModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
